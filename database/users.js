6c5dadad67d60fdbd045c67b40480ad5e9866b74
Hiroshi <mnakasone3@my.bcit.ca>
2025-01-27 18:34:10 -0800

    sql injection prone

--- database/users.js
+++ database/users.js
@@ -1,72 +1,69 @@
 const database = include('databaseConnection');
 
 async function createUser(postData) {
-	let createUserSQL = `
-		INSERT INTO user
-		(username, password)
-		VALUES
-		(:user, :passwordHash);
-	`;
+   
+    let createUserSQL = `
+        INSERT INTO user
+        (username, password)
+        VALUES
+        ('${postData.user}', '${postData.hashedPassword}');
+    `;
 
-	let params = {
-		user: postData.user,
-		passwordHash: postData.hashedPassword
-	}
-	
-	try {
-		const results = await database.query(createUserSQL, params);
+    try {
+        const results = await database.query(createUserSQL);
 
         console.log("Successfully created user");
-		console.log(results[0]);
-		return true;
-	}
-	catch(err) {
-		console.log("Error inserting user");
+        console.log(results[0]);
+        return true;
+    }
+    catch(err) {
+        console.log("Error inserting user");
         console.log(err);
-		return false;
-	}
+        return false;
+    }
 }
 
 async function getUsers(postData) {
-	let getUsersSQL = `
-		SELECT username, password
-		FROM user;
-	`;
-	
-	try {
-		const results = await database.query(getUsersSQL);
+    
+    let getUsersSQL = `
+        SELECT username, password
+        FROM user;
+    `;
+
+    try {
+        const results = await database.query(getUsersSQL);
 
         console.log("Successfully retrieved users");
-		console.log(results[0]);
-		return results[0];
-	}
-	catch(err) {
-		console.log("Error getting users");
+        console.log(results[0]);
+        return results[0];
+    }
+    catch(err) {
+        console.log("Error getting users");
         console.log(err);
-		return false;
-	}
+        return false;
+    }
 }
-
 async function getUser(postData) {
-	let getUserSQL = `
-		SELECT user_id, username, password, type
-		FROM user
-		JOIN user_type USING (user_type_id)
-		WHERE username = '${postData.user}';
-	`;
-	
-	try {
-		const results = await database.query(getUserSQL);
+    
+    let getUserSQL = `
+        SELECT user_id, username, password, type
+        FROM user
+        JOIN user_type USING (user_type_id)
+        WHERE username = '${postData.user}';
+    `;
+
+    try {
+        const results = await database.query(getUserSQL);
 
         console.log("Successfully found user");
-		console.log(results[0]);
-		return results[0];
-	}
-	catch(err) {
-		console.log("Error trying to find user");
+        console.log(results[0]);
+        return results[0];
+    }
+    catch(err) {
+        console.log("Error trying to find user");
         console.log(err);
-		return false;
-	}
+        return false;
+    }
 }
 
 module.exports = {createUser, getUsers, getUser};
--- databaseConnection.js
+++ databaseConnection.js
@@ -6,8 +6,8 @@ const dbConfig = {
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
 	database: process.env.MYSQL_DATABASE,
-	multipleStatements: false,
-	namedPlaceholders: true
+	multipleStatements: true,
+	namedPlaceholders: false
 };
 
 var database = mysql.createPool(dbConfig);
